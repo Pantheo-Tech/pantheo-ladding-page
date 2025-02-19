51d61ee4bac20f73f7b7339faa87721ddce124f4
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-02-19 09:04:16 -0300

    responsividade form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -101,8 +101,8 @@ export default function ContactForm() {
         onSubmit={handleSubmit(onSubmit)}
         className="bg-black text-white rounded-3xl w-full lg:w-1/2 p-6 sm:p-10 flex flex-col gap-6"
       >
-        <div className="flex sm:flex-row gap-4">
-          <div className="flex flex-col gap-4 w-full sm:w-2/3">
+        <div className="flex max-sm:flex-col gap-4">
+          <div className="flex flex-col gap-4 w-full sm:w-2/3 ">
             <Input
               label="Nome"
               type="text"
@@ -139,7 +139,7 @@ export default function ContactForm() {
             />
           </div>
 
-          <div className="mt-4">
+          <div className="mt-4 ">
             <label className="text-white font-medium">
               Quais serviços você precisa?
             </label>
